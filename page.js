1530198d945456d902fd398e553f08b5cd22f23d
stilist <stilist@ratafia.info>
2009-04-08 16:19:40 -0400

    Big changes: add 'dom' namespace; move paged nav creation to pagering.init; fix some math; add column-gap to calculations; adjust nav for decision that there is only one set of arrows

--- page.js
+++ page.js
@@ -1,24 +1,56 @@
 /* Provided under the MIT license. See LICENSE for details */
 
-function $(elemId) {
- return document.getElementById(elemId);
+var dom = {
+  $: function(elemId) {
+    return document.getElementById(elemId);
+  },
+  addDiv: function(name, parent) {
+    // prevent fastback cache from re-adding the element on load
+    var existing = this.$(name);
+    if (existing === null) {
+      var el = document.createElement('div');
+      el.setAttribute('id', name);
+      parent.appendChild(el);
+    }
+
+    return this.$(name);
+  },
+  addText: function(text, parent) {
+    var el = document.createTextNode(text);
+    parent.appendChild(el);
+  },
+  getCSSValue: function(element, property) {
+    var cs = document.defaultView.getComputedStyle(element, null);
+    return cs.getPropertyValue(property);
+  }
 }
 
 function debug(message) {
- debug = $('debug');
+ debug = dom.$('debug');
  debug.innerHTML += message + '<br>';
 }
 
 var pagering = {
+ // create paging nav
+ // XXX: should move the current onload code in here
+ init: function(elem) {
+  var content = dom.$(elem);
+
+  var pageNav = dom.addDiv('pagenav', content);
+  var pageLeft = dom.addDiv('pageleft', pageNav);
+  dom.addText('←', pageLeft);
+  var pageRight = dom.addDiv('pageright', pageNav);
+  dom.addText('→', pageRight);
+ },
  changePage: function(direction, totalPages, pageWidth) {
-  var content = $('content');
-  var text = $('text');
+  var content = dom.$('content');
+  var text = dom.$('text');
   var totalWidth = pageWidth * totalPages;
   /* returns e.g. '-30px', so we have to convert it from a String to an Integer */
   var leftMargin = text.style.marginLeft;
-  var currentOffset = leftMargin === '' ? 0 : parseInt(leftMargin.split('px')[0]);
+  var currentOffset = leftMargin.length == 0 ? 0 : parseInt(leftMargin.split('px')[0]);
 
-  var currentPageNumber = parseInt($('currentpage').innerHTML);
+  var currentPageNumber = parseInt(dom.$('currentpage').innerHTML);
 
   if (direction === 'l') {
    if (Math.abs(currentOffset) >= pageWidth) {
@@ -28,48 +60,72 @@ var pagering = {
    }
 
   } else {
-   if (Math.abs(currentOffset) < (totalWidth - pageWidth)) {
+   if (Math.abs(currentOffset) < totalWidth) {
     text.style.marginLeft = (currentOffset - pageWidth) + 'px';
 
     this.setCurrent(currentPageNumber + 1);
    }
   }
  },
- setCount: function(totalPages) {
-  var pageCount = $('totalpages');
-  pageCount.innerHTML = totalPages;
+ CSSDimensionToInt: function(dimension, defaultValue) {
+  var dimension = dimension.split('px')[0];
+  // if the dimension has not been set (e.g., it's 'auto', 'normal', ...), hand
+  // back defaultValue
+  return parseInt(dimension) === NaN ? defaultValue : parseInt(dimension);
+ },
+ getColumnGap: function(element) {
+  var columnGap = '';
+
+  /* surely there's a way to compress this */
+  if (dom.getCSSValue(element, 'column-gap') !== null) {
+    columnGap = dom.getCSSValue(element, 'column-gap');
+  } else if (dom.getCSSValue(element, '-webkit-column-gap') !== null) {
+    columnGap = dom.getCSSValue(element, '-webkit-column-gap');
+  } else if (dom.getCSSValue(element, '-moz-column-gap') !== null) {
+    columnGap = dom.getCSSValue(element, '-moz-column-gap');
+  }
+
+  // CSS3 spec suggests 1em as the default column gap; use 13px as an average
+  return this.CSSDimensionToInt(columnGap, 13);
+ },
+ setPageCount: function(totalPages) {
+  var pageCount = dom.$('totalpages');
+  pageCount.innerHTML = Math.round(totalPages);
  },
  setCurrent: function(pageNumber) {
-  var currentPage = $('currentpage');
+  var currentPage = dom.$('currentpage');
   currentPage.innerHTML = pageNumber;
  }
 }
 
 window.onload = function () {
- /* go into page mode and set the page count */
- var fullHeight = $('text').clientHeight;
+ var text = dom.$('text');
 
- var content = $('content');
+ /* go into page mode and set the page count */
+ var fullHeight = text.clientHeight;
+
+ var content = dom.$('content');
  content.setAttribute('class', 'pageringd');
 
- var text = $('text');
-
  var pagedHeight = text.clientHeight;
- var totalPages = Math.round(fullHeight / pagedHeight);
- pagering.setCount(totalPages);
+ var totalPages = fullHeight / pagedHeight;
+ pagering.setPageCount(totalPages);
 
+ // page width has two parts:
+ // 1) the width of the paged element
+ // 2) CSS3 column-gap of the paged element
  var pageWidth = text.clientWidth;
+ pageWidth += pagering.getColumnGap(text);
 
- /* watch for page changes */
- var leftArrow = document.getElementsByClassName('pageleft');
- for (var i = 0; i < leftArrow.length; i++) {
-  leftArrow[i].addEventListener('click',
-      function() { pagering.changePage('l', totalPages, pageWidth); }, false);
- }
+ if (totalPages > 1) {
+  pagering.init('content');
 
- var rightArrow = document.getElementsByClassName('pageright');
- for (var i = 0; i < rightArrow.length; i++) {
-  rightArrow[i].addEventListener('click',
-      function() { pagering.changePage('r', totalPages, pageWidth); }, false);
+  var leftArrow = dom.$('pageleft');
+  leftArrow.addEventListener('click',
+       function() { pagering.changePage('l', totalPages, pageWidth); }, false );
+ 
+  var rightArrow = dom.$('pageright');
+  rightArrow.addEventListener('click',
+       function() { pagering.changePage('r', totalPages, pageWidth); }, false );
  }
 }
